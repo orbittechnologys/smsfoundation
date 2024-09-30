5cfecd941ef474be76cd233c88d01cfb79253b79
orbittechnologys <145333647+orbittechnologys@users.noreply.github.com>
2024-09-30 18:57:35 +0530

    Update Content.jsx

--- src/pages/Student/Content.jsx
+++ src/pages/Student/Content.jsx
@@ -33,20 +33,48 @@ const Content = () => {
         </h1>
       </div>
       <div className="flex justify-center items-center my-5 w-full">
-        <div className="grid lg:grid-cols-2 sm:grid-cols-1 place-items-center w-1/2 gap-5">
+        <div className="grid lg:grid-cols-2 sm:grid-cols-1 justify-center items-start w-1/2 gap-5">
           {chapter?.videoUrl ? (
-            <div
-            className="flex flex-col justify-center items-center border p-5 rounded-xl cursor-pointer shadow-md shadow-orange-200"
-            onClick={() => window.open(chapter?.videoUrl, "_blank")}
-          >
-            <p className="text-xl font-semibold text-gray-400">Watch a Video</p>
-            <img src={VideoFilespng} alt="" className="lg:h-64 sm:h-36" />
-          </div>
-          ): ``
-          }
-          
+            <>
+              <div className="p-4 bg-white rounded-2xl shadow-md max-h-[330px] overflow-y-scroll">
+                <div className="mb-4">
+                  <h2 className="text-lg font-semibold text-gray-800 sticky">
+                    Available Videos
+                  </h2>
+                </div>
+                {chapter?.videoUrl.length > 0 ? (
+                  chapter?.videoUrl.map((url, index) => (
+                    <a
+                      key={index}
+                      className="flex items-center gap-3 p-3 mb-2 bg-gray-100 rounded-lg shadow-sm hover:bg-gray-200 transition"
+                      href={url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <span className="font-medium text-gray-700">
+                        {index + 1}.
+                      </span>
+                      <a
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-500 underline hover:text-blue-700 transition"
+                      >
+                        {chapter?.name} {index + 1}
+                      </a>
+                    </a>
+                  ))
+                ) : (
+                  <p className="text-gray-600">No videos available.</p>
+                )}
+              </div>
+            </>
+          ) : (
+            ``
+          )}
+
           <div
-            className="flex flex-col justify-center items-center border p-5 rounded-xl cursor-pointer shadow-md shadow-orange-200"
+            className="flex flex-col justify-center items-center border p-5 rounded-xl cursor-pointer shadow-md shadow-orange-200 "
             onClick={() => navigate(`/pdf/${chapter?._id}`)}
           >
             <p className="text-xl font-semibold text-gray-400">Read a PDF</p>
